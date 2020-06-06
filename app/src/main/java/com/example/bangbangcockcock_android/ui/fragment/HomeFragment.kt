fbeb5688a11bcd987d180024c37e6fcb0baa3ccc
yunakim2 <98_02@naver.com>
2020-06-07 08:59:25 +0900

    [유나] 서버연결 (카테고리)

--- app/src/main/java/com/example/bangbangcockcock_android/ui/fragment/HomeFragment.kt
+++ app/src/main/java/com/example/bangbangcockcock_android/ui/fragment/HomeFragment.kt
@@ -2,16 +2,25 @@ package com.example.bangbangcockcock_android
 
 import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.annotation.RequiresApi
+import com.example.bangbangcockcock_android.adapter.CategoryRecyclerAdapter
 import com.example.bangbangcockcock_android.adapter.LikeAdapter
 import com.example.bangbangcockcock_android.data.LikeData
+import com.example.bangbangcockcock_android.data.responseCategory
+import com.example.bangbangcockcock_android.network.RequestToServer
 import com.example.bangbangcockcock_android.recent_item.RecentAdapter
 import com.example.bangbangcockcock_android.recent_item.RecentData
+import com.example.bangbangcockcock_android.util.HorizontalItemDecorator
+import com.example.bangbangcockcock_android.util.VerticalItemDecorator
+import kotlinx.android.synthetic.main.fragment_category3.*
 import kotlinx.android.synthetic.main.fragment_home.*
+import retrofit2.Call
+import retrofit2.Callback
 
 /**
  * A simple [Fragment] subclass.
@@ -33,10 +42,30 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        likeAdapter =
-            LikeAdapter(view.context)
-        rv_like_home.adapter = likeAdapter
-        loadLikeDatas()
+        val requestToServer= RequestToServer
+        requestToServer.service.getConcert()
+            .enqueue(
+                object : Callback<responseCategory> {
+                    override fun onFailure(call: Call<responseCategory>, t: Throwable) {
+                        Log.d("통신실패", "${t}")
+                    }
+
+                    override fun onResponse(
+                        call: Call<responseCategory>,
+                        response:retrofit2.Response<responseCategory>
+                    ) {
+                        if (response.isSuccessful) {
+                            likeAdapter  = LikeAdapter(view.context,response.body()!!.data)
+                            rv_like_home.adapter = likeAdapter
+                            rv_like_home.addItemDecoration(VerticalItemDecorator(28))
+                            rv_like_home.addItemDecoration(HorizontalItemDecorator(16))
+
+                        }
+
+                    }
+                }
+            )
+
 
         recentAdapter = RecentAdapter(view.context)
         rv_recent_home.adapter = recentAdapter
@@ -109,8 +138,7 @@ class HomeFragment : Fragment() {
                 )
             )
         }
-        likeAdapter.datas = datas
-        likeAdapter.notifyDataSetChanged()//
+
     }
 
 }
